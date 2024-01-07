f06fb82b3d2d7d0af5124d3bd66471f5e9069e99
lcabs <39957000+lcabs@users.noreply.github.com>
2024-01-06 22:39:21 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,28 +1,29 @@
 #include <WiFi.h>
-#include <Wire.h>
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_GFX.h>
 #include <PubSubClient.h>
-
-/* Defines do Display */
-#define OLED_WIDTH 128
-#define OLED_HEIGHT 64
-#define OLED_ADDR   0x3C
+#include <DHT.h>
+#include <time.h> // Include the time library
 
 const char* ssid = "EG8115V5";
 const char* password = "alpha147147";
-const char* mqtt_server = "broker.hivemq.com";
-const char* mqtt_topic1 = "lcabs1993/ESP32/remote";
-const char* mqtt_topic2 = "lcabs1993/arduino";
+const char* mqtt_server = "192.168.18.186";
+const char* ntp_server = "pool.ntp.org"; // NTP server to get the time
+const char* mqtt_username = "your_mqtt_username"; // Replace with your MQTT username
+const char* mqtt_password = "your_mqtt_password"; // Replace with your MQTT password
 
-const char* screens[] = {"debug", "weather", "animation", "clock", "sensor", "log"};
-int currentScreen = 0;
+const int DHT_PIN = 5;  // DHT11 sensor on GPIO4
+const int SOIL_MOISTURE_PIN_1 = 36;
+const int SOIL_MOISTURE_PIN_2 = 39;
+const int SOIL_MOISTURE_PIN_3 = 34; // Additional soil moisture sensor
+const int SOIL_MOISTURE_PIN_4 = 35; // Additional soil moisture sensor
+const int RAIN_SENSOR_PIN_1 = 32;
+const int RAIN_SENSOR_PIN_2 = 33; // Additional rain sensor
+const int RELAY_PIN_1 = 16; // GPIO pin for Relay 1
+const int RELAY_PIN_2 = 17; // GPIO pin for Relay 2
+const int RELAY_PIN_3 = 18; // Additional GPIO pin for Relay 3
+const int RELAY_PIN_4 = 19; // Additional GPIO pin for Relay 4
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-//Adafruit_SSD1306 display(128, 64, &Wire, -1);
-Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT);
+DHT dht(DHT_PIN, DHT11);
+PubSubClient client;
 
 void setup() {
   Serial.begin(115200);
@@ -35,65 +36,127 @@ void setup() {
   }
   Serial.println("Connected to WiFi");
 
-  // Connect to MQTT
+  // Set the NTP server
+  configTime(0, 0, ntp_server);
+
+  // Connect to MQTT with username and password
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 
+  // Initialize sensors
+  dht.begin();
 
+  // Initialize Relay pins
+  pinMode(RELAY_PIN_1, OUTPUT);
+  pinMode(RELAY_PIN_2, OUTPUT);
+  pinMode(RELAY_PIN_3, OUTPUT); // Additional GPIO pin for Relay 3
+  pinMode(RELAY_PIN_4, OUTPUT); // Additional GPIO pin for Relay 4
 
-
-  // Initialize the OLED display
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;);
-  }
-  delay(2000);
-  display.clearDisplay();
-  display.display();
-
-  // Initial screen
-  drawScreen();
+  // Initial state: Relays are turned off
+  digitalWrite(RELAY_PIN_1, LOW);
+  digitalWrite(RELAY_PIN_2, LOW);
+  digitalWrite(RELAY_PIN_3, LOW); // Initial state for Relay 3
+  digitalWrite(RELAY_PIN_4, LOW); // Initial state for Relay 4
 }
-
 
 void loop() {
   if (!client.connected()) {
     reconnect();
   }
-
   client.loop();
+
+  // Read sensor values and publish to specific MQTT topics
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (!isnan(temperature) && !isnan(humidity)) {
+    publishSensorData("DHT11/temp", String(temperature));
+    publishSensorData("DHT11/humi", String(humidity));
+  }
+
+  int soilMoisture1 = analogRead(SOIL_MOISTURE_PIN_1);
+  int soilMoisture2 = analogRead(SOIL_MOISTURE_PIN_2);
+  int soilMoisture3 = analogRead(SOIL_MOISTURE_PIN_3); // Additional soil moisture sensor
+  int soilMoisture4 = analogRead(SOIL_MOISTURE_PIN_4); // Additional soil moisture sensor
+
+  publishSensorData("solo1", String(soilMoisture1));
+  publishSensorData("solo2", String(soilMoisture2));
+  publishSensorData("solo3", String(soilMoisture3)); // Publish data for Additional soil moisture sensor
+  publishSensorData("solo4", String(soilMoisture4)); // Publish data for Additional soil moisture sensor
+
+  int rainSensor1 = digitalRead(RAIN_SENSOR_PIN_1);
+  int rainSensor2 = digitalRead(RAIN_SENSOR_PIN_2);
+
+  publishSensorData("chuva1", String(rainSensor1));
+  publishSensorData("chuva2", String(rainSensor2));
+
+  delay(5000); // Adjust the delay based on your application requirements
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Received message on topic: ");
   Serial.println(topic);
 
-  // Convert payload to a string
   payload[length] = '\0'; // Null-terminate the payload
   String payloadStr = String((char*)payload);
 
-  // Check if payload matches any screen names
-  for (int i = 0; i < sizeof(screens) / sizeof(screens[0]); i++) {
-    if (payloadStr.equals(screens[i])) {
-      currentScreen = i;
-      Serial.print("Changing to screen: ");
-      Serial.println(screens[i]);
-      drawScreen();
-      break;
+  Serial.println("Payload: " + payloadStr);
+
+  // Check if the payload corresponds to controlling relays
+  if (String(topic) == "lcabs1993/ESP32/rele") {
+    // Control Relay 1
+    if (payloadStr == "on") {
+      digitalWrite(RELAY_PIN_1, HIGH);
+    } else if (payloadStr == "off") {
+      digitalWrite(RELAY_PIN_1, LOW);
+    }
+  } else if (String(topic) == "lcabs1993/ESP32/rele2") {
+    // Control Relay 2
+    if (payloadStr == "on") {
+      digitalWrite(RELAY_PIN_2, HIGH);
+    } else if (payloadStr == "off") {
+      digitalWrite(RELAY_PIN_2, LOW);
+    }
+  } else if (String(topic) == "lcabs1993/ESP32/control/rele3") {
+    // Control Relay 3
+    if (payloadStr == "on") {
+      digitalWrite(RELAY_PIN_3, HIGH);
+    } else if (payloadStr == "off") {
+      digitalWrite(RELAY_PIN_3, LOW);
+    }
+  } else if (String(topic) == "lcabs1993/ESP32/rele4") {
+    // Control Relay 4
+    if (payloadStr == "on") {
+      digitalWrite(RELAY_PIN_4, HIGH);
+    } else if (payloadStr == "off") {
+      digitalWrite(RELAY_PIN_4, LOW);
     }
   }
 }
 
 void reconnect() {
-  // Loop until we're reconnected
   while (!client.connected()) {
     Serial.println("Attempting MQTT connection...");
-    
-    // Attempt to connect
-    if (client.connect("ESP32Client")) {
+
+    if (client.connect("ESP32Client", mqtt_username, mqtt_password)) {
       Serial.println("Connected to MQTT broker");
-      client.subscribe(mqtt_topic1);
-      client.subscribe(mqtt_topic2);
+
+      // Get current time and adjust for GMT-3
+      struct tm timeinfo;
+      getLocalTime(&timeinfo);
+      timeinfo.tm_hour -= 3; // Adjust for GMT-3
+
+      char isoTime[20];
+      strftime(isoTime, sizeof(isoTime), "%Y-%m-%dT%H:%M:%SZ", &timeinfo);
+
+      String connectionMessage = "Successfully connected to MQTT broker at " + String(isoTime);
+      client.publish("lcabs1993/ESP32", connectionMessage.c_str());
+
+      // Subscribe to control topics
+      client.subscribe("lcabs1993/ESP32/rele1");
+      client.subscribe("lcabs1993/ESP32/rele2");
+      client.subscribe("lcabs1993/ESP32/rele3");
+      client.subscribe("lcabs1993/ESP32/rele4");
     } else {
       Serial.print("MQTT connection failed, rc=");
       Serial.println(client.state());
@@ -102,17 +165,7 @@ void reconnect() {
   }
 }
 
-void drawScreen() {
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-
-  // Draw the screen title
-  display.print("Screen: ");
-  display.println(screens[currentScreen]);
-
-  // Add additional screen-specific content here
-
-  display.display();
+void publishSensorData(const char* sensorName, const String& sensorValue) {
+  String topic = "lcabs1993/ESP32/" + String(sensorName);
+  client.publish(topic.c_str(), sensorValue.c_str());
 }
